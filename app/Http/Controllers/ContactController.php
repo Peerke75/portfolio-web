767132bba16d55ab6c6980c2c129cfad3af58791
Peerke75 <d297354@edu.curio.nl>
2024-09-05 10:58:49 +0200

    contact pagina afgerond voor mij uit te kunnen nodigen

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ContactSubmission;
+
+class ContactController extends Controller
+{
+    public function sendContactForm(Request $request)
+    {
+        // Validate the form data
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string'
+        ]);
+
+        // Save the contact form submission to the database
+        ContactSubmission::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'subject' => $request->input('subject'),
+            'message' => $request->input('message'),
+        ]);
+
+        // Redirect back with a success message
+        return back()->with('success', 'JE uitnodeging is succesvol verstuurd!');
+    }
+}
+            
